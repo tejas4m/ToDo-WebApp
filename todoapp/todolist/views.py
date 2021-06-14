dee5f3dc35322cd2b0ca415a7b99bc113da4aeb1
tejas4m <tejas4202@gmail.com>
2021-06-15 00:10:30 +0530

    create task

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import *
-
+from .forms import *
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+@csrf_exempt
 def index(request):
     tasks = Task.objects.all()
 
-    context = { 'tasks' : tasks}
-    return render(request , 'todolist/list.html' , context)
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')  
+
+    context = { 'tasks' : tasks, 'form' : form}
+    return render(request , 'todolist/list.html' , context )
