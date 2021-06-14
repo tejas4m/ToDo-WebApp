4984b77adcb548153fde508028bde4d2b74bccbe
tejas4m <tejas4202@gmail.com>
2021-06-14 16:37:52 +0530

    models

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello world')
+    tasks = Task.objects.all()
+
+    context = { 'tasks' : tasks}
+    return render(request , 'todolist/list.html' , context)
